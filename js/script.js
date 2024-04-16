f21d87ce7616cb16c2533dd74fc627c4952ed10c
Falos732 <alexyouellet1@gmail.com>
2024-04-15 23:21:47 -0400

    base

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function page2_click() {
+    window.location.href = "http://127.0.0.1:5500/html/page2.html";
+}
+
+function home() {
+    window.location.href = "http://127.0.0.1:5500/html/index.html";
+}
