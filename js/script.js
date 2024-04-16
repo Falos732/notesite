592764a24999bb474ce3eceea92e86e8afb27e52
Falos732 <alexyouellet1@gmail.com>
2024-04-16 14:54:31 -0400

    meilleur bouton

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ function reseau_click() {
     window.location.href = "http://127.0.0.1:5500/html/reseau.html";
 }
 
+function os_click() {
+    window.location.href = "http://127.0.0.1:5500/html/os.html";
+}
+
 function soutien_click() {
     window.location.href = "http://127.0.0.1:5500/html/soutien.html";
 }
