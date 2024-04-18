d58c08f4fe7a3396781bacfa573befe253442067
Falos732 <alexyouellet1@gmail.com>
2024-04-17 20:16:02 -0400

    ip correct

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function programmation_click() {
-    window.location.href = "http://127.0.0.1:5500/html/programmation.html";
+    window.location.href = "http://192.168.10.177:5500/html/programmation.html";
 }
 
 function reseau_click() {
-    window.location.href = "http://127.0.0.1:5500/html/reseau.html";
+    window.location.href = "http://192.168.10.177:5500/html/reseau.html";
 }
 
 function os_click() {
-    window.location.href = "http://127.0.0.1:5500/html/os.html";
+    window.location.href = "http://192.168.10.177:5500/html/os.html";
 }
 
 function soutien_click() {
-    window.location.href = "http://127.0.0.1:5500/html/soutien.html";
+    window.location.href = "http://192.168.10.177:5500/html/soutien.html";
 }
 
 function home_click() {
-    window.location.href = "http://127.0.0.1:5500/html/index.html";
+    window.location.href = "http://192.168.10.177:5500/html/index.html";
 }
