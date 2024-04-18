ef9f8721203c1844438ead6581e69d72a8f8b0e6
Falos732 <alexyouellet1@gmail.com>
2024-04-17 21:35:15 -0400

    ip modif

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function programmation_click() {
-    window.location.href = "http://192.168.10.177:5500/html/programmation.html";
+    window.location.href = "http://192.168.10.177/html/programmation.html";
 }
 
 function reseau_click() {
-    window.location.href = "http://192.168.10.177:5500/html/reseau.html";
+    window.location.href = "http://192.168.10.177/html/reseau.html";
 }
 
 function os_click() {
-    window.location.href = "http://192.168.10.177:5500/html/os.html";
+    window.location.href = "http://192.168.10.177/html/os.html";
 }
 
 function soutien_click() {
-    window.location.href = "http://192.168.10.177:5500/html/soutien.html";
+    window.location.href = "http://192.168.10.177/html/soutien.html";
 }
 
 function home_click() {
-    window.location.href = "http://192.168.10.177:5500/html/index.html";
+    window.location.href = "http://192.168.10.177/html/index.html";
 }
